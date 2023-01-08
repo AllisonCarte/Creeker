21792913fd9b77566e738819b471fe918a13b3ae
Allison Carte <cartemack@gmail.com>
2023-01-07 23:08:47 -0500

    Post SQL

---
+++ Post Seed Data.sql
@@ -0,0 +1,17 @@
+﻿--INSERT INTO post (userId, categoryId, title, content, imageLocation, createDateTime, publishDateTime, isApproved, isEmergency)
+--VALUES (1, 5, 'Stay safe during the holidays', 'The holidays can be a busy and stressful time. Follow these tips to keep yourself and your loved ones safe...', 'https://example.com/holiday-safety.jpg', '2022-01-01 12:00:00', '2022-01-01 12:00:00', 1, 0);
+
+--INSERT INTO post (userId, categoryId, title, content, imageLocation, createDateTime, publishDateTime, isApproved, isEmergency)
+--VALUES (2, 2, 'Winter weather warning', 'A winter storm is expected to bring snow and icy conditions to the region. Please take precautions and stay informed...', 'https://example.com/winter-storm.jpg', '2022-01-02 12:00:00', '2022-01-02 12:00:00', 1, 1);
+
+--INSERT INTO post (userId, categoryId, title, content, imageLocation, createDateTime, publishDateTime, isApproved, isEmergency)
+--VALUES (3, 1, 'Holiday travel tips', 'Planning to travel for the holidays? Here are some helpful tips to make your trip go smoothly...', 'https://example.com/holiday-travel.jpg', '2022-01-03 12:00:00', '2022-01-03 12:00:00', 1, 0);
+
+--INSERT INTO post (userId, categoryId, title, content, imageLocation, createDateTime, publishDateTime, isApproved, isEmergency)
+--VALUES (4, 1, 'Healthy holiday eating', 'It is easy to overindulge during the holidays, but it is important to maintain a healthy diet. Here are some tips to help you stay on track...', 'https://example.com/holiday-eating.jpg', '2022-01-04 12:00:00', '2022-01-04 12:00:00', 1, 0);
+
+--INSERT INTO post (userId, categoryId, title, content, imageLocation, createDateTime, publishDateTime, isApproved, isEmergency)
+--VALUES (5, 5, 'Winter driving safety', 'Winter weather can make driving treacherous. Follow these tips to stay safe on the roads...', 'https://example.com/winter-driving.jpg', '2022-01-05 12:00:00', '2022-01-05 12:00:00', 1, 0);
+
+--INSERT INTO post (userId, categoryId, title, content, imageLocation, createDateTime, publishDateTime, isApproved, isEmergency)
+--VALUES (10, 1, 'Holiday gift ideas', 'Looking for the perfect gift for your loved ones? Check out these creative and thoughtful ideas...', 'https://example.com/gift-ideas.jpg', '2022-01-06 12:00:00', '2022-01-06 12:00:00', 0, 0);
